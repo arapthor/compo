a18eafdfb1109fada7f1d32bc2eadd2d2dcf101c
arapthor <67490853+arapthor@users.noreply.github.com>
2023-01-15 11:04:59 -0400

    compo

---
+++ datos.js
@@ -0,0 +1,263 @@
+const data = [{
+    title: 'Atmel-ATTINY45-20SU',
+    description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    date: 'Last updated 3 mins ago',
+    img: '.png',
+    link: 'module',
+    specifications: {
+        primero: { value: 450, desc: 'lorem ipsum tors' },
+        segundo: { value: '90-190', desc: 'lorem ipsum tors' },
+        tercero: { value: 8.4, desc: 'lorem ipsum tors' },
+        cuarto: { value: '1320-1760', desc: 'lorem ipsum tors' },
+        quinto: { value: '8S2', desc: 'lorem ipsum tors' }
+    },
+    specs: {
+        freq: {
+            id: 'Frequency',
+            value: '20 MHz'
+        },
+        mem: {
+            id: 'Memory Size',
+            value: '8 kB'
+        },
+        memtype: {
+            id: 'Memory Type',
+            value: 'FLASH'
+        },
+        temp: {
+            id: 'Operating Temperature',
+            value: '-40°C - 85°C'
+        },
+        volt: {
+            id: 'Supply Voltage',
+            value: '2.7V - 5.5V'
+        },
+        io: {
+            id: 'Number of Programmable I/O',
+            value: '6'
+        },
+        ram: {
+            id: 'RAM Size',
+            value: '512 B'
+        }
+    },
+    sheets: {
+        prov1: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        },
+        prov2: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        },
+        prov3: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        },
+        prov4: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        },
+        prov5: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        }
+    }
+}, {
+    title: 'Atmel-ATTINY85-20SUR',
+    description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    date: 'Last updated 3 mins ago',
+    img: '.png',
+    link: 'module',
+    specifications: {
+        primero: { value: 360, desc: 'lorem ipsum tors' },
+        segundo: { value: '30-100', desc: 'lorem ipsum tors' },
+        tercero: { value: 6, desc: 'lorem ipsum tors' },
+        cuarto: { value: '1320-1760', desc: 'lorem ipsum tors' },
+        quinto: { value: '8S2', desc: 'lorem ipsum tors' }
+    },
+    specs: {
+        freq: {
+            id: 'Frequency',
+            value: '20 MHz'
+        },
+        mem: {
+            id: 'Memory Size',
+            value: '8 kB'
+        },
+        memtype: {
+            id: 'Memory Type',
+            value: 'FLASH'
+        },
+        temp: {
+            id: 'Operating Temperature',
+            value: '-40°C - 85°C'
+        },
+        volt: {
+            id: 'Supply Voltage',
+            value: '2.7V - 5.5V'
+        },
+        io: {
+            id: 'Number of Programmable I/O',
+            value: '6'
+        },
+        ram: {
+            id: 'RAM Size',
+            value: '512 B'
+        }
+    },
+    sheets: {
+        prov1: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        },
+        prov2: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        },
+        prov3: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        },
+        prov4: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        },
+        prov5: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        }
+    }
+}, {
+    title: 'Microchip-ATTINY45-20SU',
+    description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    date: 'Last updated 3 mins ago',
+    img: '.jpg',
+    link: 'module',
+    specifications: {
+        primero: { value: 50, desc: 'lorem ipsum tors' },
+        segundo: { value: '10-140', desc: 'lorem ipsum tors' },
+        tercero: { value: 4.35, desc: 'lorem ipsum tors' },
+        cuarto: { value: '1320-1760', desc: 'lorem ipsum tors' },
+        quinto: { value: '8S2', desc: 'lorem ipsum tors' }
+    },
+    specs: {
+        freq: {
+            id: 'Frequency',
+            value: '20 MHz'
+        },
+        mem: {
+            id: 'Memory Size',
+            value: '8 kB'
+        },
+        memtype: {
+            id: 'Memory Type',
+            value: 'FLASH'
+        },
+        temp: {
+            id: 'Operating Temperature',
+            value: '-40°C - 85°C'
+        },
+        volt: {
+            id: 'Supply Voltage',
+            value: '2.7V - 5.5V'
+        },
+        io: {
+            id: 'Number of Programmable I/O',
+            value: '6'
+        },
+        ram: {
+            id: 'RAM Size',
+            value: '512 B'
+        }
+    },
+    sheets: {
+        prov1: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        },
+        prov2: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        },
+        prov3: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        },
+        prov4: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        },
+        prov5: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        }
+    }
+}, {
+    title: 'Microchip-ATTINY25-20SSU',
+    description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    date: 'Last updated 3 mins ago',
+    img: '.jpg',
+    link: 'module',
+    specifications: {
+        primero: { value: 450, desc: 'lorem ipsum tors' },
+        segundo: { value: '90-190', desc: 'lorem ipsum tors' },
+        tercero: { value: 8.4, desc: 'lorem ipsum tors' },
+        cuarto: { value: '1320-1760', desc: 'lorem ipsum tors' },
+        quinto: { value: '8S2', desc: 'lorem ipsum tors' }
+    },
+    specs: {
+        freq: {
+            id: 'Frequency',
+            value: '20 MHz'
+        },
+        mem: {
+            id: 'Memory Size',
+            value: '8 kB'
+        },
+        memtype: {
+            id: 'Memory Type',
+            value: 'FLASH'
+        },
+        temp: {
+            id: 'Operating Temperature',
+            value: '-40°C - 85°C'
+        },
+        volt: {
+            id: 'Supply Voltage',
+            value: '2.7V - 5.5V'
+        },
+        io: {
+            id: 'Number of Programmable I/O',
+            value: '6'
+        },
+        ram: {
+            id: 'RAM Size',
+            value: '512 B'
+        }
+    },
+    sheets: {
+        prov1: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        },
+        prov2: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        },
+        prov3: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        },
+        prov4: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        },
+        prov5: {
+            title: 'datasheet 1 ',
+            value: '/pdct/module.html'
+        }
+    }
+}]
+
+exports = data;
---
+++ search.js
@@ -0,0 +1,63 @@
+const display = document.querySelector('#display');
+
+var copia = [];
+
+const search = (valued) => {
+    const search = document.getElementById('search').value.toUpperCase();
+    //console.log(search);
+    //const List = document.getElementById('display');
+    //console.log(List)
+    const items = document.querySelectorAll('.card');
+    //console.log(items);
+    //const pname = List.getElementsByTagName('h5');
+    //console.log(pname);
+    var top = true;
+    var value;
+
+
+
+    if (valued) {
+        value = valued.toUpperCase();
+        document.getElementById('search').value = valued;
+    } else {
+        value = search;
+    }
+
+    for (let index = 0; index < data.length; index++) {
+        let match = data[index].title;
+        console.log('prove');
+        if (match.toUpperCase().indexOf(value) !== -1 && value !== ' ' && value !== '') {
+            copia.push(data[index]);
+
+        }
+    }
+    /*for (let index = 0; index < items.length; index++) {
+        let match = items[index].getElementsByTagName('h5')[0];
+        let text = match.textContent || match.innerHTML;
+
+        if (text.toUpperCase().indexOf(value) == -1 || value == '') {
+            items[index].style.display = "none";
+
+        } else {
+            items[index].style.display = "";
+            window.scrollTo(0, screen.height / 2.4);
+            top = false;
+        }
+
+    }*/
+
+    if (copia.length > 0) {
+        window.scrollTo(0, screen.height / 2.4);
+    } else {
+        window.scrollTo(0, 0);
+    }
+
+    results();
+    copia = [];
+}
+
+window.onload = () => {
+    if (document.getElementById('search')) { search(); };
+}
+
+exports = copia;
